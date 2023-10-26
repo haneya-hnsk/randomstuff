424d743f5637c375a99e42ec5ea37519b2e05133
haneya-hnsk <106055064+haneya-hnsk@users.noreply.github.com>
2023-10-26 13:43:07 +0700

    hehehee

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace sarpras_dimasr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        classKoneksi koneksi = new classKoneksi();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            MySqlConnection conn = koneksi.mysqlkoneksi();
+            if (conn.State == ConnectionState.Open)
+            {
+
+
+                // MessageBox.Show("nyala");
+            }
+            else
+            {
+                MessageBox.Show("mati");
+
+            }
+
+        }
+        public static string level = "";
+        private void login_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection conn = koneksi.mysqlkoneksi();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM user Where username='" + textBox1.Text + "' and password='" + textBox2.Text + "'", conn);
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                level = dt.Rows[0]["level"].ToString();
+                if (level == "admin") {
+                    MessageBox.Show("hai admin");
+
+                }
+                else if (level == "manajemen")
+                {
+                    MessageBox.Show("hai Manajemen");
+
+                }
+                else if (level == "peminjam")
+                {
+                    MessageBox.Show("hai Peminjam");    
+
+                }
+
+                formMenu form = new formMenu();
+               form.Show();
+               this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("login gagal");
+
+            }
+        }
+
+       
+    }
+}
---
+++ formMenu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sarpras_dimasr
+{
+    public partial class formMenu : Form
+    {
+        public formMenu()
+        {
+            InitializeComponent();
+
+            string level = Form1.level;
+            //MessageBox.Show(level);
+            if (level == "mananjemen")
+            {
+                pinjamBarangToolStripMenuItem.Visible = false;
+
+            }
+
+            else if (level == "admin")
+            {
+
+                laporanToolStripMenuItem.Visible = false;
+
+            }
+            else if (level == "peminjam")
+            {
+                dataBarangToolStripMenuItem.Visible = false;
+                laporanToolStripMenuItem.Visible = false;
+                suplierToolStripMenuItem.Visible = false;
+                barangMasukToolStripMenuItem.Visible = false;
+                barangKeluarToolStripMenuItem.Visible = false;
+
+
+            }
+        }
+
+        private void formMenu_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataBarangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formBarang fb = new formBarang();
+          //  this.show();
+            fb.Show();
+        }
+
+        private void pinjamBarangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formPinjamBarang pb = new formPinjamBarang();
+           pb.Show();
+        }
+
+        private void barangMasukToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formBarangMasuk fb = new formBarangMasuk();
+            // this.Show();
+            fb.Show();
+        }
+
+        private void suplierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formSuplier fs = new formSuplier();
+            fs.Show();
+        }
+
+        private void barangKeluarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formBarangKeluar fs = new formBarangKeluar();
+            fs.Show();
+        }
+    }
+}
